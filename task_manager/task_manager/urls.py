ae4d14d614ccf9d63cdbcdf598cb854e8b325402
deorz <TheDamnation1044@yandex.ru>
2022-06-05 17:59:47 +0300

    Добавлен requirements.txt + документация к API

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SC_TaskManager API",
+        default_version='v1',
+        description=("Документация для приложения API "
+                     "проекта ИС'Диспетчер Суперкомпьютера'"),
+        contact=openapi.Contact(email="TheDamnation1044@yandex.ru"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('api/', include('api.urls', namespace='api')),
     path('admin/', admin.site.urls),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),
 ]
